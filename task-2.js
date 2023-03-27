43bf1b654121dbceb073edebd51a94f60d548ddc
ShriduttPatel10 <shridutt.patel@openxcell.com>
2023-03-27 21:11:02 +0530

    added comments for reference

--- task-2.js
+++ task-2.js
@@ -1,3 +1,4 @@
+// TaskRunner.runTask method
 function RandomPromises(taskId) {
     const randomNumber = Math.random() * 4500;
     return new Promise((resolve, reject) => {
@@ -21,6 +22,7 @@ const proFillArray = [];
 tasks.forEach((t, idx) => {
     const index = idx;
     promises.push(RandomPromises(t).then(() => {
+        // saving the resolving task order in the array
         proFillArray.push(index)
     }));
 });
@@ -32,8 +34,10 @@ const result = [];
 const set = new Set();
 proFillArray.forEach((tc) => {
     for(let i = 0; i < tc; i++) {
+        // checking wether prior tasks are completed or not
         if(!set.has(i)) {
-            result[tc] = -1
+            // save it as -1 if the prior task are not completed
+            result[tc] = -1;
             set.add(tc);
             return;
         }
@@ -45,6 +49,7 @@ console.log(result);
 }
 
 run();
+
 // added method for shuffling the array
 Array.prototype.shuffle = function shuffle() {
     this.sort(() => Math.random() - 0.5);
